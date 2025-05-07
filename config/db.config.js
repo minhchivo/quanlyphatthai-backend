a187c1f571435b6c9d009df6199c9015d00b24c0
minhchivo <vominhchi2109@gmail.com>
2025-05-07 07:11:52 +0700

    reupdate db.config

--- config/db.config.js
+++ config/db.config.js
@@ -3,11 +3,14 @@ const mysql = require('mysql2');
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,           // Thêm dòng này nếu bạn chưa có
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: true // ⚠️ Bắt buộc khi kết nối qua Railway Public
+  ssl: {
+    rejectUnauthorized: true // hoặc false nếu gặp lỗi chứng chỉ tự ký
+  }
 });
+
 
 module.exports = db;
