4e6316ba5c97e2310a0b0b7c39ddce74244d8b4e
minhchivo <vominhchi2109@gmail.com>
2025-05-07 07:06:17 +0700

    change db.config

--- config/db.config.js
+++ config/db.config.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-// Tạo pool kết nối MySQL
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,           // Thêm dòng này nếu bạn chưa có
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: true // ⚠️ Bắt buộc khi kết nối qua Railway Public
 });
 
 module.exports = db;
